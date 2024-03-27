c66aeb4108f60e65f7743da7850ab8a22b6ba39b
Varma1708 <148996332+Varma1708@users.noreply.github.com>
2024-03-27 22:38:14 +0530

    Update RewardValue.java

--- rewards-converter/src/main/java/RewardValue.java
+++ rewards-converter/src/main/java/RewardValue.java
@@ -1,32 +1,28 @@
 public class RewardValue {
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-        private double cashValue;
-        private int milesValue;
-
-        // Constructor accepting cash value
-        public RewardValue(double cashValue) {
-            this.cashValue = cashValue;
-            // Convert cash value to miles
-            this.milesValue = (int) (cashValue / 0.0035);
-        }
-
-        // Constructor accepting miles value
-        public RewardValue(int milesValue) {
-            this.milesValue = milesValue;
-            // Convert miles to cash value
-            this.cashValue = milesValue * 0.0035;
-        }
-
-        // Method to get cash value
-        public double getCashValue() {
-            return cashValue;
-        }
-
-        // Method to get miles value
-        public int getMilesValue() {
-            return milesValue;
-        }
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
 
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    }
 
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
+}
